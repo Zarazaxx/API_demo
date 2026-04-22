ca6f787390e9403aa23a605d483e0c548f3cce2f
ZarazaXX <meduza1995shtin@gmail.com>
2026-04-22 18:16:20 +0700

    1. Add AllureReport 2. Add Ci pipeline

--- src/test/java/spec/BaseApi.java
+++ src/test/java/spec/BaseApi.java
@@ -1,5 +1,6 @@
 package spec;
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.given;
@@ -18,7 +19,8 @@ public abstract class BaseApi {
 
         protected RequestSpecification request() {
             RequestSpecification spec = given()
-                    .spec(BaseSpecs.baseRequestSpec());
+                    .spec(BaseSpecs.baseRequestSpec())
+                    .filter(new AllureRestAssured());
 
             if (token != null) {
                 spec.auth().oauth2(token);
